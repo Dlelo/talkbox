3885d8a88dd99c53aa8730549d2070a5f98dcd47
donattah <donattah@donattahs-MacBook-Air.local>
2021-08-10 15:47:12 +0300

    update token reducer file

--- src/store/Reducers/tokenReducer.js
+++ src/store/Reducers/tokenReducer.js
@@ -9,7 +9,6 @@ export default function tokenReducer(state = initialState, action) {
     case types.TOKEN:
         return {...state};
     case types.TOKEN_SUCCESS:
-        console.log(action, "action====")
         return {...state, token: action};
     case types.TOKEN_FAILS:
         return {...state, error: action.error}
