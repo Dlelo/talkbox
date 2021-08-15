69093f4aa566cdbf934b93d10adc662fb028d057
donattah <donattah@donattahs-MacBook-Air.local>
2021-08-15 16:14:27 +0300

    displaying speech text to the UI

--- src/components/SingleChatMic.jsx
+++ src/components/SingleChatMic.jsx
@@ -13,6 +13,7 @@ const SingleChatMic = props =>{
     const [msg, setMessage] = useState('Speak...');
     const [isValidToken , setIsValidToken] = useState(false)
     const token_timout_duration = 10*60*1000;
+    const [showMsg, setShowMsg] = React.useState(false)
 
     let micColor
  
@@ -34,45 +35,43 @@ const SingleChatMic = props =>{
      if(msg === "Speak ..."){
         micColor = "red"
      }
-        function speechFromMic() {
-            const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(token, 'centralus');
-            speechConfig.speechRecognitionLanguage = 'en-Us';       
-            const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
-            const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
-            console.log(ResultReason.RecognizedSpeech, "the result reason recognizer result")
-            recognizer.recognizeOnceAsync(result => {
-                 console.log(result, "the speech recognizer result")
-                 let displayText;
-                 if (result.reason === ResultReason.RecognizedSpeech) {
-                     console.log(result, 'recognized speech results')
-                     displayText = `RECOGNIZED: Text=${result.text}`
-                     setMessage(result?.text)
-                 } else {
-                     displayText = result?.errorDetails + ' ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
-                     setMessage(displayText)
-                     console.log('error', result?.errorDetails);
-                 }
-                 
-             });
-         }
+    function speechFromMic() {
+        //setShowMsg(true)
+        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(token, 'centralus');
+        speechConfig.speechRecognitionLanguage = 'en-Us';       
+        const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
+        const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+        console.log(ResultReason.RecognizedSpeech, "the result reason recognizer result")
+        recognizer.recognizeOnceAsync(result => {
+            console.log(result, "the speech recognizer result")
+            let displayText;
+            if (result.reason === ResultReason.RecognizedSpeech) {
+                console.log(result, 'recognized speech results')
+                displayText = `RECOGNIZED: Text=${result.text}`
+                setMessage(result?.text)
+            } else {
+                displayText = result?.errorDetails + ' ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
+                setMessage(displayText)
+                console.log('error', result?.errorDetails);
+            }
+            
+        });
+    }
    return (
        <Container>
            <Row>
-            <Card className="chat-card" Style={{ border:'none' }}>
+            <Card className="chat-card" style={{ border:'none' }}>
+                {/* { showMsg ?  <Card className="displayTheText"><p>{msg}</p></Card> : null } */}
+                <Card className="displayTheText"><p>{msg}</p></Card>
                <Row style={{ bottom: 0, width:'100%', padding: '0.4rem', position: 'fixed', alignContent:'center' }}>
                   <Col  xs={5} sm={5} md={5} lg={5}>
                   </Col>
                   <Col  xs={4} sm={4} md={4} lg={4}>
-                  <button onClick={speechFromMic()} color={micColor} className="micButton" style={{backgroundColor:'rgb(63,131,214)', borderRadius:'100%', padding:'3px', border:'0', borderColor:'rgb(63,131,214)'}}><Mic color="#ccc"  size={30}/></button>
+                    <button onClick={speechFromMic()} className="micButton"><Mic color="#ccc"  size={30}/></button>
                   </Col>
                   <Col  xs={3} sm={3} md={3} lg={3}>
                   </Col>
 
-               </Row>
-               <Row className="displayTheText">
-                   <Card className="displayTheText">
-                   <code className="displayTheText">{msg}</code>
-                   </Card>
                </Row>
             </Card>
             </Row>
