b893912286cc35bc9637ec95a1f51042a3848ee2
donattah <akinyidonattah@gmail.com>
2021-08-17 10:14:43 +0300

    creating store and reducers

--- src/store/Reducers/index.js
+++ src/store/Reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import tokenReducer from '../Reducers/tokenReducer'
+import chatsReducer from './chatsReducer';
 
 const rootReducer = combineReducers({
     token : tokenReducer,
+    chats : chatsReducer,
 });
 
 export default rootReducer;
